cf7eafe09f54d4e52cd030c03221808e97c02487
James Kim <90013871+jamesktkim@users.noreply.github.com>
2022-01-21 16:34:51 -0800

    Update clean_up.R

--- R/clean_up.R
+++ R/clean_up.R
@@ -1,12 +1,17 @@
-#' Takes a dataframe object and returns a cleaned version with the NaN values removed. It also prints a list of potential outliers for each explanatory variable, based on a threshold distance from the respective median value.
+#' Takes a dataframe object and returns a cleaned version with rows containing any NaN values dropped. Inspects the clean dataframe and prints a list of potential outliers for each explanatory variable, based on the threshold distance of 3 standard deviations.
 #'
 #' @param df The dataframe on which the function will operate
 #'
-#' @return the same dataframe with all the NaN's removed
+#' @return the same dataframe with all the NaN's removed along with a list of potential outliers
 #' @export
 #'
 #' @examples
 #' library(palmerpenguins)
 #' results <- clean_up(penguins)
+#' '**The following potenital outliers were detected:**
+#' Variable X: 
+#' [ 300, 301, 500, 1000 ]
+#' Variable Y: 
+#' [ 6.42, 6.44, 58.52, 60.22 ]'
 clean_up <- function(df) {
 }
