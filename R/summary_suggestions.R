45bf1d55c024277d68ec8ef1ca64fe5cca73e42b
Luming21 <lumingyang.ut@gmail.com>
2022-01-28 01:33:54 -0800

    Revised to pass tests in branch, should not be merged into main

--- R/summary_suggestions.R
+++ R/summary_suggestions.R
@@ -1,25 +1,17 @@
 #' Takes a dataframe object and returns a list object comprising
 #' of 3 dataframes and a list. The dataframes correspond to the
 #' summary statistics of numeric and categorical variables each and
-#' the proportion of unique values for categorical variables. The 
+#' the proportion of unique values for categorical variables. The
 #' nested list is of the categorical variables that exceed the threshold
 #' for considering dropping variables with high unique values.
 #'
 #' @param df The dataframe on which the function will operate
-#' @param threshold A float. If passed, it will define the threshold for identifying the variables with high unique values, defaults to 0.8
-#'
-#' @return
+#' @return table
 #' @export
 #'
 #' @examples
 #' library(palmerpenguins)
 #' summary_suggestions(penguins)
-#' [
-#' (summary statistics for numeric variables),
-#' (summary statistics for categorical variables),
-#' (percentage of unique values for categorical variables),
-#' [list of variables with percentage of unique values higher than the threshold]
-#' ]
-    
+
 summary_suggestions <- function(df) {
 }
