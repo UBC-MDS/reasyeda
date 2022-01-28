0b7971decd547676067f3d641c7ce6df4100fff8
James Kim <james.ktkim@gmail.com>
2022-01-28 03:13:47 -0800

    Adding clean_up test file

--- tests/testthat/test-clean_up.R
+++ tests/testthat/test-clean_up.R
@@ -1,3 +1,29 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+
+test_that("Testing if a list of outliers is produced", {
+  test_df <- data.frame(
+    a=c(1,1,1,1,1,1,1,1,1,1,1e14),
+    b=c(NA,'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i','j')
+  )
+  expect_type(clean_up(test_df), "list")
+})
+
+test_that("Testing if dataframe is returned", {
+  test_df <- data.frame(
+    a=c(1,1,1,1,1,1,1,1,1,1,1e14),
+    b=c(NA,'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i','j')
+  )
+  expect_s3_class(clean_up(test_df), "data.frame")
+})
+
+test_that("Testing for correct outlier detected", {
+  test_df <- data.frame(
+    a=c(1,1,1,1,1,1,1,1,1,1,1e14),
+    b=c(NA,'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i','j')
+  )
+  expect_snapshot(clean_up(test_df), 1e+14)
+})
+
+test_that("Checking errors in input", {
+  test_list <- list(1,1,1,1,1,1,1,1,1,1,1e14)
+  expect_error(clean_up(test_list))
 })
